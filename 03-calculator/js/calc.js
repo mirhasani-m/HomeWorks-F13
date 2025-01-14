6814436adfef9fdd00514e21e19a48d2b1011476
mirhasani-m <mirhasani.m69@gmail.com>
2025-01-15 01:53:36 +0330

    switch -oparatorType

--- 03-calculator/js/calc.js
+++ 03-calculator/js/calc.js
@@ -1,37 +1,56 @@
 const scr = document.querySelector('.screen');
-let screenNumber = '0' , partA , partB , oparatorType , helper = 0 , result ;
-scr.innerHTML = screenNumber ;
-function printOnScreen(x){
-  if( x == "." ){
-    scr.innerHTML = (+screenNumber).toLocaleString() + "." ;
-  }else{
-    scr.innerHTML = (+screenNumber).toLocaleString() ;
+let screenNumber = '0', partA, partB, oparatorType, helper = 0, result;
+scr.innerHTML = screenNumber;
+function printOnScreen(x) {
+  if (x == ".") {
+    scr.innerHTML = (+screenNumber).toLocaleString() + ".";
+  } else {
+    scr.innerHTML = (+screenNumber).toLocaleString();
   }
 }
-function setNum(x){
-  if (helper){
-    screenNumber = '' ;
-    helper = 0 ;
+function setNum(x) {
+  if (helper) {
+    screenNumber = '';
+    helper = 0;
   }
-  if (screenNumber == '0'){
+  if (screenNumber == '0') {
     screenNumber = '';
   }
-  screenNumber = screenNumber + x ;
+  screenNumber = screenNumber + x;
   printOnScreen(x)
 }
-function operate(type){
-partA = +screenNumber ;
-oparatorType = type ;
-helper = 1 ;
+function operate(type) {
+  if (partA) {
+    calculate()
+  } else {
+    partA = +screenNumber;
+  }
+  oparatorType = type;
+  helper = 1;
 
 }
-function calculate(){
-  partB = +screenNumber ;
-if (oparatorType == '+'){
-  result = partA + partB ;
-}
+function calculate() {
+    partB = +screenNumber;
+  if (partB) {
+    switch (oparatorType) {
+      case "+":
+        result = partA + partB;
+        break;
+      case "-":
+        result = partA - partB;
+        break;
+      case "*":
+        result = partA * partB;
+        break;
 
-scr.innerHTML = result ;
+      default:
+        result = partA / partB;
+    }
+  }
+  partA = result;
+  helper = 1;
+  scr.innerHTML = result;
+
 }
 
 
